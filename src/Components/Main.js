70cdf86eb9020ed96ea229702e02908161fcee2d
Daniel Gude <dgude31@gmail.com>
2021-08-04 05:37:00 -0400

    Meat and Potatoes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as PropTypes from "prop-types";
 
 function Main(props) {
@@ -6,10 +6,35 @@ function Main(props) {
     hashItems: { access_token },
   } = props;
 
+  const [responseCode, setResponseCode] = useState(null);
+
   return (
-    <button className="shuffleButton" onClick={() => {}}>
-      Activate Shuffle
-    </button>
+    <div>
+      <button
+        className="shuffleButton"
+        onClick={() => {
+          fetch("https://api.spotify.com/v1/me/player/shuffle?state=true", {
+            method: "PUT",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${access_token}`,
+            },
+            body: "",
+          })
+            .then((response) => setResponseCode(response.status))
+            .catch(console.error);
+        }}
+      >
+        Activate Shuffle
+      </button>
+      {responseCode &&
+        (responseCode === 204 ? (
+          <p className="regText successText">Yay, your playback has been shuffled :)</p>
+        ) : (
+          <p className="regText errorText">It looks like there was an error :(</p>
+        ))}
+    </div>
   );
 }
 
