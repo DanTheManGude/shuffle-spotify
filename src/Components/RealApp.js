579a25b1d9bac57a6bee47f78631e8219696ffb8
Daniel Gude <dgude31@gmail.com>
2021-08-04 03:07:40 -0400

    Some button

--- src/Components/RealApp.js
+++ src/Components/RealApp.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function RealApp() {
+  const search = useLocation().search;
+
   return (
     <div className="RealApp">
       <p className="regText">some auth stuff I guess</p>
-      <button className="regText" onClick={() => console.log("Lemme get that auth")}>
+      <button className="regText authButton" onClick={() => console.log("Lemme get that auth")}>
         Get auth
       </button>
     </div>
